bc3ce9a2d7bbcc3077988072086881d296cfef59
Daniel <danielsolano1000@yahoo.com.br>
2020-04-20 21:00:41 -0300

    Versão inicial

---
+++ Analise_mercado_financeiro_com_R.R
@@ -0,0 +1,48 @@
+# Big Data na Prática - Análise de Séries Temporais no Mercado Financeiro
+
+setwd("D:/Desenvolvimento/_Exercícios/BigDataRAzure")
+getwd()
+
+# http://www.quantmod.com
+
+
+# Pacotes
+install.packages("quantmod")
+install.packages("xts")
+install.packages("moments")
+library(quantmod)
+library(xts) # Permite trabalhar com xts (série temporal)
+library(moments)
+
+
+# Seleção do período de análise
+startDate = as.Date("2019-03-20")
+endDate = as.Date("2020-03-20")
+
+
+# Download dos dados do período
+papel <- getSymbols("LREN3.SA", src = "yahoo", from = startDate, to = endDate, auto.assign = F)
+class(papel)
+
+
+# Visualizando os dados
+head(papel)
+View(papel)
+
+
+# Plot do fechamento
+papel.Adjusted <- papel[, 6]
+papel.Adjusted <- na.omit(papel.Adjusted)
+
+plot(papel.Adjusted, main = "Fechamento Diário do preço da Ação",
+     col = "red", xlab = "Data", ylab = "Preço", major.ticks = 'months',
+     minor.ticks = FALSE)
+
+
+# Gráfico de candlestick da ação
+candleChart(papel)
+
+
+# Adicionado as bandas de bollinger ao gráfico, com média de 20 períodos e 2 desvios padrão
+addBBands(n = 20, sd = 2)
+
